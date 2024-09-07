d14507c3bb9747a86409c19fb6d3bfcb8d97af24
thinascimento3 <thi030507sn@gmail.com>
2024-09-06 23:09:55 -0300

    Update app.py
    correções

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 ##  primeiro projeto  sala de cadastro da agenda de shows  
 ## banda the Vulteres
 
-print ('Olá bemm vindo a sala de cadastros')
+print ('Olá bem vindo a sala de cadastros')
 nome=input('qual o seu nome?')
 
 print ('ola',nome, 'seja bem vindo')
-data=input('qual a data de nascimennto?')
+data=input('qual a data de nascimento?')
 email=input('qual o se email?')
 contato=int(input('qual seu numero de contato?'))
 evento=input('qual a data desejada para o evento?')
-hrs=input('qual será o horario do evento?')
+hrs=input('qual será o horário do evento?')
 print('ok já estamos com seus dados.')
-print (nome, 'sua data de nascimento é',data,'seu email é',email, 
-       'e seu contao',contato, 'e a data do evennto será dia:',evento)
-print ('Assim que possível entraremmos e contato para maiorres informações')
+print (nome, 'sua data de nascimento é',data,'seu email ',email, 
+       'e seu contao',contato, ' a data do evennto será dia:',evento)
+print ('Assim que possível entraremos e contato para maiores informações')
 print ('A banda the Vulteres agradece o seu contato, obrigado')
